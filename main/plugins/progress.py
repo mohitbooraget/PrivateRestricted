6660fdf0a5c680aa95fc8f113c7916ed4008a8e1
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-06-07 19:16:11 +0530

    Update progress.py

--- main/plugins/progress.py
+++ main/plugins/progress.py
@@ -3,8 +3,8 @@ import os
 import time
 import json
 
-FINISHED_PROGRESS_STR = "█"
-UN_FINISHED_PROGRESS_STR = ""
+FINISHED_PROGRESS_STR = "▓"
+UN_FINISHED_PROGRESS_STR = "▒"
 DOWNLOAD_LOCATION = "/app"
 
 
